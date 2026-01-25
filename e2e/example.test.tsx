59fbcdb824b7f39049145c2a55e2dc84cfdc9b6d
Kiryshka1922 <kipabelousov@edu.hse.ru>
2026-01-25 22:57:16 +0300

    infra/playwright-config: updated workflow for launching tests

--- e2e/example.test.tsx
+++ e2e/example.test.tsx
@@ -1,7 +1,11 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test.describe("Meeting Form", () => {
   test.beforeEach(async ({ page }) => {
     await page.goto("/");
+  });
+
+  test("check", () => {
+    expect(true).toBeTruthy();
   });
 });
