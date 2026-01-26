ac71797216784b825b18cd6e80b53029c901712d
Kiryshka1922 <kipabelousov@edu.hse.ru>
2026-01-26 11:09:09 +0300

    infra/playwright-config: updated config

--- playwright-ct.config.ts
+++ playwright-ct.config.ts
@@ -61,11 +61,11 @@ export default defineConfig({
       use: { ...devices["Desktop Safari"] },
     },
     {
-      name: "mobile-chrome",
+      name: "Mobile Chrome",
       use: { ...devices["Pixel 5"] },
     },
     {
-      name: "mobile-safari",
+      name: "Mobile Safari",
       use: { ...devices["iPhone 12"] },
     },
   ],
