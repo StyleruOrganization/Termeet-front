6eb0c09cba2ae93a26718a7c5ce8345d2db36ba3
Kiryshka1922 <kipabelousov@edu.hse.ru>
2026-02-03 15:39:41 +0300

    feature/calendar-for-select-time: realise highliting pretty easyX5

--- src/components/TableColumn/TableColumn.tsx
+++ src/components/TableColumn/TableColumn.tsx
@@ -7,7 +7,6 @@ export const TableColumn = ({ cells }: TableColumnProps) => {
   const [isSelecting, setIsSelecting] = useState(false);
   const [isRemoving, setIsRemoving] = useState(false);
   const [selectedCells, setSelectedCells] = useState<Set<string | null>>(new Set<string>());
-  const beginDataId = useRef<string | null>(null);
 
   const handlePointerUp = () => {
     console.log("select end TableColumn");
@@ -21,26 +20,35 @@ export const TableColumn = ({ cells }: TableColumnProps) => {
     setIsRemoving(isRemoving);
   };
 
-  console.log("SelectedSells", selectedCells);
-
   return (
     <div
       onTouchStart={e => {
-        e.preventDefault();
         const touch = e.touches[0];
         const element = document.elementFromPoint(touch.clientX, touch.clientY) as HTMLElement;
         if (!element) return;
         const dataId = element.getAttribute("data-id");
         console.log("tpuchStart in tableColumn", dataId);
-        beginDataId.current = dataId;
 
         setIsSelecting(true);
         if (dataId && selectedCells.has(dataId)) {
           setIsRemoving(true);
         }
+
+        if (isRemoving) {
+          setSelectedCells(prev => {
+            const newSet = new Set(prev);
+            newSet.delete(dataId);
+            return newSet;
+          });
+        } else {
+          setSelectedCells(prev => {
+            const newSet = new Set(prev);
+            newSet.add(dataId);
+            return newSet;
+          });
+        }
       }}
       onTouchMove={e => {
-        e.preventDefault();
         const touch = e.touches[0];
         const element = document.elementFromPoint(touch.clientX, touch.clientY);
         if (!element) return;
@@ -61,21 +69,7 @@ export const TableColumn = ({ cells }: TableColumnProps) => {
         console.log("touchMove in touchCollumn", dataId);
       }}
       onTouchEnd={e => {
-        e.preventDefault();
         console.log("touchEnd in touchColumn");
-        if (isRemoving) {
-          setSelectedCells(prev => {
-            const newSet = new Set(prev);
-            newSet.delete(beginDataId.current);
-            return newSet;
-          });
-        } else {
-          setSelectedCells(prev => {
-            const newSet = new Set(prev);
-            newSet.add(beginDataId.current);
-            return newSet;
-          });
-        }
 
         setIsSelecting(false);
         setIsRemoving(false);
