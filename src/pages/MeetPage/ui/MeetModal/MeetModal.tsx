8993ba4abe26668822ba330f30e65bace86ec0e5
Kirill Belousov <kipabelousov@edu.hse.ru>
2026-03-09 23:46:01 +0300

    feature/new-react-hook-for-form: added useActionState (#94)

--- src/pages/MeetPage/ui/MeetModal/MeetModal.tsx
+++ src/pages/MeetPage/ui/MeetModal/MeetModal.tsx
@@ -1,55 +1,68 @@
-import { useRef, useState } from "react";
+import { useActionState } from "react";
 import { useParams } from "react-router";
 import { ModalWrapper, Input } from "@/shared/ui";
 import { useMeetContext } from "@entities/Meet";
 import { useSaveUserSelectedData } from "./../../api";
 import styles from "./MeetModal.module.css";
 
+type FormState = {
+  error: string | null;
+  userName: string;
+};
+
 export const MeetModal = () => {
   const { hash } = useParams();
   const isOpen = useMeetContext(state => state.isModalOpen);
   const setIsOpen = useMeetContext(state => state.setIsModalOpen);
   const setIsEditingMode = useMeetContext(store => store.setIsEditing);
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [error, setError] = useState<string>("");
   const { mutate } = useSaveUserSelectedData(hash || "", () => {
     setIsEditingMode(false);
     setIsOpen(false);
-    setError("");
   });
+  const [state, formAction, isPending] = useActionState<FormState, FormData>(
+    (prevState, formData) => {
+      console.log("call useActionState", prevState, formData);
 
-  const handleSelectedSlots = () => {
-    const inputEl = inputRef.current;
-    if (!inputEl) return;
+      const userName = formData.get("userName")?.toString().trim();
 
-    if (inputEl.value.trim() === "") {
-      setError("Необходимо ввести ваше имя");
-      return;
-    }
+      console.log("userName", userName);
 
-    mutate(inputEl.value.trim());
-  };
+      if (!userName || userName === "") {
+        return {
+          error: "Необходимо ввести ваше имя",
+          userName: prevState.userName || "",
+        };
+      }
+
+      mutate(userName);
+
+      return {
+        error: null,
+        userName,
+      };
+    },
+    {
+      error: null,
+      userName: "",
+    },
+  );
 
   return (
     <>
       <ModalWrapper isAnimate animationDuration={300} isOpen={isOpen} onClose={() => setIsOpen(false)}>
-        <div data-test-id='meet-modal' className={styles.MeetModal}>
+        <form action={formAction} data-test-id='meet-modal' className={styles.MeetModal}>
           <h3 className={styles.MeetModal__Title}>Введите ваше имя</h3>
           <Input
-            error={error}
-            inputRef={inputRef}
+            error={(!state.userName && state.error) || ""}
             placeholder='Иван Иванов'
             label='Введите ваше имя'
             name='userName'
-            onChange={() => {
-              setError("");
-            }}
             autoComplete='given-name'
           />
-          <button onClick={handleSelectedSlots} className={styles.MeetModal__SaveButton}>
+          <button disabled={isPending} className={styles.MeetModal__SaveButton}>
             Сохранить
           </button>
-        </div>
+        </form>
       </ModalWrapper>
     </>
   );
