5c9711784fb182d856c68dc180976da80d94613e
Kiryshka1922 <kipabelousov@edu.hse.ru>
2026-03-27 22:16:59 +0300

    104-create-loader: added message for loader

--- src/shared/ui/Loader/Loader.tsx
+++ src/shared/ui/Loader/Loader.tsx
@@ -6,36 +6,39 @@ export const Loader = () => {
   const cells = Array.from({ length: 35 });
 
   return (
-    <div className={styles.loader}>
-      <div className={styles.header}>
-        <div className={`${styles.pill} ${styles.pillLeft}`} />
+    <div className={styles.loader__wrapper}>
+      <div className={styles.loader}>
+        <div className={styles.header}>
+          <div className={`${styles.pill} ${styles.pillLeft}`} />
 
-        <div className={styles.logo}>
-          <LogoIcon />
+          <div className={styles.logo}>
+            <LogoIcon />
+          </div>
+
+          <div className={`${styles.pill} ${styles.pillSmall}`} />
+          <div className={`${styles.pill} ${styles.pillSmall}`} />
         </div>
+        <div className={styles.gridContainer}>
+          {cells.map((_, index) => {
+            const rowIndex = Math.floor(index / 7);
+            const colIndex = index % 7;
 
-        <div className={`${styles.pill} ${styles.pillSmall}`} />
-        <div className={`${styles.pill} ${styles.pillSmall}`} />
+            return (
+              <div
+                key={index}
+                className={styles.cell}
+                style={
+                  {
+                    "--row": rowIndex,
+                    "--col": colIndex,
+                  } as React.CSSProperties
+                }
+              />
+            );
+          })}
+        </div>
       </div>
-      <div className={styles.gridContainer}>
-        {cells.map((_, index) => {
-          const rowIndex = Math.floor(index / 7);
-          const colIndex = index % 7;
-
-          return (
-            <div
-              key={index}
-              className={styles.cell}
-              style={
-                {
-                  "--row": rowIndex,
-                  "--col": colIndex,
-                } as React.CSSProperties
-              }
-            />
-          );
-        })}
-      </div>
+      <div className={styles.loader__message}>Загружаем слоты ...</div>
     </div>
   );
 };
