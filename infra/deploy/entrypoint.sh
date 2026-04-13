180015e94620c93a630123126932de20522b00f2
Kiryshka1922 <kipabelousov@edu.hse.ru>
2026-04-13 19:49:12 +0300

    some corrections for deploy

--- infra/deploy/entrypoint.sh
+++ infra/deploy/entrypoint.sh
@@ -2,14 +2,13 @@
 set -e
 
 echo "=== Starting nginx with env variables ==="
-echo "BACKEND_CONTAINER: ${BACKEND_CONTAINER}"
 echo "BACKEND_PORT: ${BACKEND_PORT}"
 echo "BACKEND_HOST: ${BACKEND_HOST}"
 echo "VIRTUAL_HOST: ${VIRTUAL_HOST}"
 echo "========================================="
 
 # Заменяем переменные в шаблоне
-envsubst '${BACKEND_CONTAINER} ${BACKEND_PORT} ${BACKEND_HOST}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${BACKEND_PORT} ${BACKEND_HOST}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
 
 # Показываем сгенерированный конфиг (для отладки)
 echo "=== Generated nginx config ==="
