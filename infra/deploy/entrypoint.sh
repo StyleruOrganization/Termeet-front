e5e0a1b6c7632f9a3bf383e065bec42651400315
Kiryshka1922 <kipabelousov@edu.hse.ru>
2026-04-13 19:34:11 +0300

    update configs and configure proxy for prod backend

---
+++ infra/deploy/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+
+echo "=== Starting nginx with env variables ==="
+echo "BACKEND_CONTAINER: ${BACKEND_CONTAINER}"
+echo "BACKEND_PORT: ${BACKEND_PORT}"
+echo "BACKEND_HOST: ${BACKEND_HOST}"
+echo "VIRTUAL_HOST: ${VIRTUAL_HOST}"
+echo "========================================="
+
+# Заменяем переменные в шаблоне
+envsubst '${BACKEND_CONTAINER} ${BACKEND_PORT} ${BACKEND_HOST}' < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+
+# Показываем сгенерированный конфиг (для отладки)
+echo "=== Generated nginx config ==="
+cat /etc/nginx/conf.d/default.conf
+echo "================================"
+
+# Проверяем конфиг
+nginx -t
+
+# Запускаем nginx
+exec nginx -g 'daemon off;'
